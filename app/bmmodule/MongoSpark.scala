3b161fdc8a780abcadecda6df3e8f39872e25808
jeorch <jeorch@foxmail.com>
2017-12-28 16:10:12 +0800

    demo for jeorch'host

--- app/bmmodule/MongoSpark.scala
+++ app/bmmodule/MongoSpark.scala
@@ -13,32 +13,32 @@ import javax.inject.Singleton
 @Singleton
 class MongoDBSpark extends SparkContextManager {
     def queryUsers(range : Range) = users
-    def queryProfiles(range : Range) = user_profile
+    def queryProfiles(range : Range) = services
 }
 
 trait SparkContextManager {
     val conf = new SparkConf()
                     .setAppName("dongda-service")
 //                    .setMaster("local[4]")
-                    .setMaster("spark://127.0.0.1:9999")
-                    .setJars("/Users/BM/.ivy2/jars/org.mongodb.spark_mongo-spark-connector_2.11-2.0.0.jar" ::
-                             "/Users/BM/.ivy2/jars/org.mongodb_mongo-java-driver-3.2.2.jar" ::  Nil)
+                    .setMaster("local")
+                    .setJars("/home/jeorch/.ivy2/cache/org.mongodb.spark/mongo-spark-connector_2.11/jars/mongo-spark-connector_2.11-2.0.0.jar" ::
+                             "/home/jeorch/.ivy2/cache/org.mongodb/mongo-java-driver/jars/mongo-java-driver-3.2.2.jar" ::  Nil)
 
     val sc = new SparkContext(conf)
 
     lazy val users = {
         val readConfig = ReadConfig(Map("spark.mongodb.input.uri" -> "mongodb://127.0.0.1/"
-                                        , "spark.mongodb.input.database" -> "baby"
+                                        , "spark.mongodb.input.database" -> "baby_time_test"
                                         , "spark.mongodb.input.collection" -> "users"
                                         , "readPreference.name" -> "secondaryPreferred"))
 //        MongoSpark.load(sc, readConfig = readConfig).sortBy(x => x.getLong("date"), false)
         MongoSpark.load(sc, readConfig = readConfig)
     }
-    
-    lazy val user_profile = {
+
+    lazy val services = {
         val readConfig = ReadConfig(Map("spark.mongodb.input.uri" -> "mongodb://127.0.0.1/"
-                                        , "spark.mongodb.input.database" -> "baby"
-                                        , "spark.mongodb.input.collection" -> "user_profile"
+                                        , "spark.mongodb.input.database" -> "baby_time_test"
+                                        , "spark.mongodb.input.collection" -> "services"
                                         , "readPreference.name" -> "secondaryPreferred"))
 //        MongoSpark.load(sc, readConfig = readConfig).sortBy(x => x.getLong("date"), false)
         MongoSpark.load(sc, readConfig = readConfig)
--- app/controllers/SparkTestController.scala
+++ app/controllers/SparkTestController.scala
@@ -11,12 +11,12 @@ import play.api.inject.Modules
 class SparkTestController @Inject() (msk : MongoDBSpark) extends Controller {
     
     def mongosparktest = Action {
-        val a = msk.user_profile.count
-        Ok(s"abcde result is $a")
+        val a = msk.services.count
+        Ok(s"services count is $a")
     }
     
     def mongosparklst = Action {
-        val a = msk.user_profile.toLocalIterator.slice(2, 6).map (x => x.getString("user_id")).toList
+        val a = msk.services.toLocalIterator.slice(2, 6).map (x => x.getString("user_id")).toList
         Ok(s"abcde result is $a")
     }
 }
